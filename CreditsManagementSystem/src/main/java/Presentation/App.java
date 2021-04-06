0436ca6cea2f32008c0b5317ff96b6a1f7744f0b
simon <sival19@student.sdu.dk>
2021-04-06 15:18:43 +0100

    javafx med maven LORTET VIRKER

--- CreditsManagementSystem/src/main/java/Presentation/App.java
+++ CreditsManagementSystem/src/main/java/Presentation/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package Presentation;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
--- CreditsManagementSystem/src/main/java/Presentation/SystemInfo.java
+++ CreditsManagementSystem/src/main/java/Presentation/SystemInfo.java
@@ -1,4 +1,4 @@
-package org.example;
+package Presentation;
 
 public class SystemInfo {
 
