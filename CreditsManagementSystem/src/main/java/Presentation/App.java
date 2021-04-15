37170de089fb278f7704d13a4e15c386b2049324
Fuad H. Farah <fujam20@student.sdu.dk>
2021-04-15 07:16:52 +0200

    Add FXML

--- CreditsManagementSystem/src/main/java/Presentation/App.java
+++ CreditsManagementSystem/src/main/java/Presentation/App.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 
 /**
@@ -27,6 +28,7 @@ public class App extends Application {
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
