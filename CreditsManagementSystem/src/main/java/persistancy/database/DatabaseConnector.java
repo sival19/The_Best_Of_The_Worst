112e5833433304471fa87a0bb5ca865a80254370
Simonas V <70447480+sival19@users.noreply.github.com>
2021-05-04 10:57:39 +0100

    connectorback

---
+++ CreditsManagementSystem/src/main/java/persistancy/database/DatabaseConnector.java
@@ -0,0 +1,46 @@
+package persistancy.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    //set connection parameters
+    private static DatabaseConnector instance;
+    private String url = "localhost";
+    private int port = 5432;
+    private String databaseName = "TV2";
+    private String username = "postgres";
+    private String password = "haleluja";
+    private Connection connection = null;
+
+    private DatabaseConnector(){
+        initializePostgresqlDatabase();
+
+    }
+    public static DatabaseConnector getInstance(){
+        if (instance == null) {
+            instance = new DatabaseConnector();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
+    private void initializePostgresqlDatabase() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection = DriverManager.getConnection("jdbc:postgresql://" + url + ":" + port + "/" + databaseName, username, password);
+        } catch (SQLException | IllegalArgumentException ex) {
+            ex.printStackTrace(System.err);
+        } finally {
+            if (connection == null) System.exit(-1);
+        }
+    }
+}
+
